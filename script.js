cda9b886d4b69941d316d6d858f5732547172a3f
ericafk0001 <dxslyofficial@gmail.com>
2024-11-28 18:46:09 -0500

    Add functionality

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const visualizeButton = document.getElementById("visualizeButton");
+  const visualizeButton = document.getElementById("visualizeBtn");
 
-  visualizeButton.addEventListener("click", () => {
-    const amount = document.getElementById("amount").value;
+  visualizeButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const amount = document.querySelector("input[type='number']").value;
     const object = document.getElementById("object").value;
-    const visualizationDiv = document.getElementById("visualization");
+    const visualizationDiv = document.getElementById("amount-div");
     visualizationDiv.innerHTML = "";
 
     let objectPrice;
 
     if (object === "penny") {
       objectPrice = 0.01;
+    } else if (object === "nickel") {
+      objectPrice = 0.05;
+    } else if (object === "dime") {
+      objectPrice = 0.1;
+    } else if (object === "quarter") {
+      objectPrice = 0.25;
+    } else if (object === "porsche911gt3rs") {
+      objectPrice = 241300;
     }
 
     const numberOfObjects = Math.floor(amount / objectPrice);
